5a23e8b241a7ff754505bada1c90c26fa859e3a8
SevenErol <lorenzo.maggi1998@gmail.com>
2022-12-21 16:31:40 +0100

    added controller, model and database table

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-
-    $comics = config('db.comics');
-
-    return view('welcome', compact('comics'));
-});
+Route::get('/', [PageController::class, 'index']);
