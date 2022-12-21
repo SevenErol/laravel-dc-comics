bf7ce55975b58afa9a31fedd21cf2c2305166baf
SevenErol <lorenzo.maggi1998@gmail.com>
2022-12-21 15:14:09 +0100

    copied laravel-comics layout

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,132 @@
+<footer>
+
+    <section id="icons_links">
+
+        <div class="container">
+
+            <div class="row row-cols-5">
+
+                <div class="col d-flex align-items-center">
+                    <img src="{{  Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    <p class="text-white ms-3">DIGITAL COMICS</p>
+                </div>
+
+                <div class="col d-flex align-items-center">
+                    <img src="{{  Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                    <p class="text-white ms-3">DC MERCHANDISE</p>
+                </div>
+
+                <div class="col d-flex align-items-center">
+                    <img src="{{  Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                    <p class="text-white ms-3">SUBSCRIPTION</p>
+                </div>
+
+                <div class="col d-flex align-items-center">
+                    <img id="locator" src="{{  Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    <p class="text-white ms-3">COMIC SHOP LOCATOR</p>
+                </div>
+
+                <div class="col d-flex align-items-center">
+                    <img src="{{  Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    <p class="text-white ms-3">DC POWER VISA</p>
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <section id="footer_center">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-6 d-flex flex-wrap">
+                    <div class="col-4 p-3">
+
+                        <h3>DC COMICS</h3>
+                        <ul class="p-0 m-0">
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+
+                    </div>
+
+                    <div class="col-4 p-3">
+
+                        <h3>DC COMICS</h3>
+                        <ul class="p-0 m-0">
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+
+                    </div>
+
+                    <div class="col-4 p-3">
+
+                        <h3>DC COMICS</h3>
+                        <ul class="p-0 m-0">
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+
+                    </div>
+
+                    <div class="col-4 p-3">
+
+                        <h3>DC COMICS</h3>
+                        <ul class="p-0 m-0">
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                        </ul>
+
+                    </div>
+                </div>
+
+
+                <div class="col-6 img_position">
+
+                    <img class="img-fluid" src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section id="footer_bottom">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-4">
+                    <button>SIGN UP NOW!</button>
+                </div>
+
+                <div class="col-6 d-flex justify-content-end align-items-center">
+                    <p class="col-2 m-0 p-0"><a href="#">FOLLOW US</a></p>
+                    <img class="ms-3" src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="">
+                    <img class="ms-3" src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+                    <img class="ms-3" src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="">
+                    <img class="ms-3" src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                    <img class="ms-3" src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,79 @@
+<header>
+
+    <section id="more">
+
+        <div class="container text-end">
+            <a href="#">DC POWER VISA &copy;</a>
+            <a href="#" class="ms-3">MORE DC SITES</a>
+        </div>
+
+    </section>
+
+    <section id="navigation">
+
+        <nav class="navbar navbar-expand-lg">
+
+            <div class="container">
+
+                <div class="logo">
+                    <img src="{{  Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                </div>
+
+                <div>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">CHARACTERS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">COMICS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">MOVIES</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">TV</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">GAMES</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">COLLECTIBLES</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">VIDEOS</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">FANS</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">NEWS</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">SHOP</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div>
+                    <input type="text" name="search" id="search" placeholder="Search...">
+                </div>
+
+            </div>
+
+        </nav>
+
+    </section>
+
+    <section id="jumbotron">
+
+    </section>
+
+</header>
