43ae4cf093060cf0103c30ac54f3a28a93056c36
SevenErol <lorenzo.maggi1998@gmail.com>
2022-12-22 15:05:04 +0100

    table on index created

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,8 +1,43 @@
-@extends ('layouts.app')
+@extends ('layouts.app_admin')
 
 @section('content')
 
+<div class="container p-3">
+    <div class="row">
 
-<h1>index php</h1>
+        <table class="table table-dark table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Thumb</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Series</th>
+                    <th scope="col">Sale date</th>
+                    <th scope="col">Type</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach ($comics as $comic)
+                <tr>
+                    <th scope="row">{{ $comic-> id}}</th>
+                    <td>{{ $comic-> title}}</td>
+                    <td>{{ $comic-> description}}</td>
+                    <td>{{ $comic-> thumb}}</td>
+                    <td>{{ $comic-> price}}</td>
+                    <td>{{ $comic-> series}}</td>
+                    <td>{{ $comic-> sale_date}}</td>
+                    <td>{{ $comic-> typeb}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+
+    </div>
+</div>
+
 
 @endsection
