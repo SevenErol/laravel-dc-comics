f67fff51919d89dec7bc9a6c7530ae18ed19fe77
SevenErol <lorenzo.maggi1998@gmail.com>
2023-01-09 15:55:03 +0100

    validation error cleaner

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -14,31 +14,52 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Comic title</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}">
+            @error('title')
+            <small id="titleHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="description">Comic description</label>
-            <textarea class="form-control" placeholder="Leave a description" id="description" name="description" style="height: 150px">{{old('description')}}</textarea>
+            <textarea class="form-control @error('description') is-invalid @enderror" placeholder="Leave a description" id="description" name="description" style="height: 150px">{{old('description')}}</textarea>
+            @error('description')
+            <small id="descriptionHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="thumb" class="form-label">Comic thumb</label>
-            <input type="url" class="form-control" id="thumb" name="thumb" value="{{old('thumb')}}">
+            <input type="url" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{old('thumb')}}">
+            @error('thumb')
+            <small id="thumbHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Comic price</label>
-            <input type="text" class="form-control" id="price" name="price" value="{{old('price')}}">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{old('price')}}">
+            @error('price')
+            <small id="priceHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="serie" class="form-label">Comic series</label>
-            <input type="text" class="form-control" id="serie" name="serie" value="{{old('serie')}}">
+            <input type="text" class="form-control @error('serie') is-invalid @enderror" id="serie" name="serie" value="{{old('serie')}}">
+            @error('serie')
+            <small id="serieHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="date" class="form-label">Comic sale date</label>
-            <input type="date" class="form-control" id="date" name="date" value="{{old('date')}}">
+            <input type="date" class="form-control @error('date') is-invalid @enderror" id="date" name="date" value="{{old('date')}}">
+            @error('date')
+            <small id="dateHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="type" class="form-label">Comic type</label>
-            <input type="text" class="form-control" id="type" name="type" value="{{old('type')}}">
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" value="{{old('type')}}">
+            @error('type')
+            <small id="typeHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -17,31 +17,52 @@
 
         <div class="mb-3">
             <label for="title" class="form-label">Comic title</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ $index-> title }}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ $index-> title }}">
+            @error('title')
+            <small id="titleHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="description">Comic description</label>
-            <textarea class="form-control" id="description" name="description" style="height: 170px">{{ $index-> description }}</textarea>
+            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" style="height: 170px">{{ $index-> description }}</textarea>
+            @error('description')
+            <small id="descriptionHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class=" mb-3">
             <label for="thumb" class="form-label">Comic thumb</label>
-            <input type="url" class="form-control" id="thumb" name="thumb" value="{{ $index-> thumb }}">
+            <input type="url" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{ $index-> thumb }}">
+            @error('thumb')
+            <small id="thumbHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Comic price</label>
-            <input type="text" class="form-control" id="price" name="price" value="{{ $index-> price }}">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ $index-> price }}">
+            @error('price')
+            <small id="priceHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="serie" class="form-label">Comic series</label>
-            <input type="text" class="form-control" id="serie" name="serie" value="{{ $index-> series }}">
+            <input type="text" class="form-control @error('serie') is-invalid @enderror" id="serie" name="serie" value="{{ $index-> series }}">
+            @error('serie')
+            <small id="serieHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="date" class="form-label">Comic sale date</label>
-            <input type="date" class="form-control" id="date" name="date" value="{{ $index-> sale_date }}">
+            <input type="date" class="form-control @error('date') is-invalid @enderror" id="date" name="date" value="{{ $index-> sale_date }}">
+            @error('date')
+            <small id="dateHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="type" class="form-label">Comic type</label>
-            <input type="text" class="form-control" id="type" name="type" value="{{ $index-> type }}">
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" value="{{ $index-> type }}">
+            @error('type')
+            <small id="typeHlper" class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
